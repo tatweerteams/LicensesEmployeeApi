96b76392ced36f709cb1cbb3a15da6313f0c5888
moh4sadek <moh4sadek@gmail.com>
2023-03-20 12:09:56 +0200

    commit

--- TatweerSendDomain/Domain/OrderItem.cs
+++ TatweerSendDomain/Domain/OrderItem.cs
@@ -9,8 +9,18 @@ namespace TatweerSendDomain.Domain
         public string AccountId { get; set; }
         public string AccountNo { get; set; }
         public string AccountName { get; set; }
+        public string AccountNameEn { get; set; }
+        public string IdentityNumber { get; set; }
+        public string NID { get; set; }
+        public string DateOfBirth { get; set; }
+        public string PlaseOfBirth { get; set; }
+        public string Address { get; set; }
+        public DateTime? IssueDate { get; set; }
+        public DateTime? ValidUpDate { get; set; }
+        public string Note { get; set; }
         public string SerialFrom { get; set; }
-        public int CountChekBook { get; set; }
+        public int CountChekBook { get; set; } = 1;
+
         public OrderItemState State { get; set; } = OrderItemState.Success;
         public DateTime CreateAt { get; set; }
         public virtual Account Account { get; set; }
